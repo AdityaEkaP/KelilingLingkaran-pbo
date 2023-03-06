7ba21897dba4cfabcc474babc5f3e7afa4751700
asus <111202113694@mhs.dinus.ac.id>
2023-03-06 23:15:44 +0700

    first commit

---
+++ keliling/kelilinglingkaran.java
@@ -0,0 +1,13 @@
+package keliling;
+
+public class kelilinglingkaran {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double k,r,d = 10,phi = 3.14;
+		r = d / 2;
+		k = 2 * phi * r;
+		System.out.print("Keliling dari Lingkaran yang berdiameter 10cm yaitu : " +k+ "cm");
+	}
+
+}
